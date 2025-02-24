020a057752c9d1114058fe7c5e072674a7be5a65
Ted <2508067350@qq.com>
2025-02-24 15:00:42 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -235,15 +235,6 @@ export function useLoadData() {
   }, []);
 }
 
-function deleteAllCookies() {
-  const cookies = document.cookie.split(";");
-
-  cookies.forEach((cookie) => {
-    const cookieName = cookie.split("=")[0].trim();
-    document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
-  });
-}
-
 export function Home() {
   useSwitchTheme();
   useLoadData();
