13f02deaae3d4f9e4d73c9edaf68d645844c6fa8
Ted <2508067350@qq.com>
2025-03-08 13:03:49 +0800

    Update api.ts

--- app/client/smarties/api.ts
+++ app/client/smarties/api.ts
@@ -4,6 +4,10 @@ export const api = axios.create({ baseURL: "" });
 
 api.interceptors.request.use(
   async (config) => {
+    console.log(
+      process.env.NEXT_PUBLIC_SMARTIES_URL,
+      "process.env.NEXT_PUBLIC_SMARTIES_URL",
+    );
     config.baseURL = process.env.NEXT_PUBLIC_SMARTIES_URL;
 
     return config;
