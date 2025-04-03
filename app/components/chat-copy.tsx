c17a8e666ce5f2d4dcdb5f638eaec252ffa926e0
Ted <2508067350@qq.com>
2025-04-03 18:16:39 +0800

    限制APP 安卓端上传文件类型

--- app/components/chat-copy.tsx
+++ app/components/chat-copy.tsx
@@ -1752,9 +1752,22 @@ export function _Chat_NEW() {
           setUploading(true);
           const files = event.target.files;
           const imagesData: string[] = [];
+          const validImageTypes = [
+            "image/png",
+            "image/jpeg",
+            "image/webp",
+            "image/heic",
+            "image/heif",
+          ];
+
           for (let i = 0; i < files.length; i++) {
             const file = event.target.files[i];
             console.log("file", file);
+
+            if (!validImageTypes.includes(file.type)) {
+              continue;
+            }
+
             uploadImageRemote(file)
               .then((dataUrl) => {
                 imagesData.push(dataUrl);
