07274435fffac5830fdd7e0c0ea8611bd054b0e5
Ted <2508067350@qq.com>
2025-04-22 19:42:21 +0800

    删除log

--- app/api/common.ts
+++ app/api/common.ts
@@ -89,7 +89,7 @@ export async function requestOpenai(req: NextRequest) {
   }
 
   const fetchUrl = cloudflareAIGatewayUrl(`${baseUrl}/${path}`);
-  console.log("fetchUrl", fetchUrl, req.headers.get("OnlineSearch"));
+  console.log("fetchUrl", fetchUrl);
   const fetchOptions: RequestInit = {
     headers: {
       "Content-Type": "application/json",
