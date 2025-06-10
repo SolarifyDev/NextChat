ea705bd2b0a7bc2e255ae4d5b2e4d97c06951a18
Ted <2508067350@qq.com>
2025-06-10 16:47:55 +0800

    Revert "audioContext 重置回Google版本"
    This reverts commit d11b4dcf79bcf5203c377118ba071beb7f19c74c.

--- app/utils/audio-utils.ts
+++ app/utils/audio-utils.ts
@@ -52,47 +52,23 @@ function unlockAudioContext(audioCtx: AudioContext) {
   }
 }
 
-export const audioContext: (
+export async function audioContext(
   options?: GetAudioContextOptions,
-) => Promise<AudioContext> = (() => {
-  const didInteract = new Promise((res) => {
-    window.addEventListener("pointerdown", res, { once: true });
-    window.addEventListener("keydown", res, { once: true });
-  });
-
-  return async (options?: GetAudioContextOptions) => {
-    try {
-      const a = new Audio();
-      a.src =
-        "data:audio/wav;base64,UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA";
-      await a.play();
-      if (options?.id && map.has(options.id)) {
-        const ctx = map.get(options.id);
-        if (ctx) {
-          return ctx;
-        }
-      }
-      const ctx = new AudioContext(options);
-      if (options?.id) {
-        map.set(options.id, ctx);
-      }
-      return ctx;
-    } catch (e) {
-      await didInteract;
-      if (options?.id && map.has(options.id)) {
-        const ctx = map.get(options.id);
-        if (ctx) {
-          return ctx;
-        }
-      }
-      const ctx = new AudioContext(options);
-      if (options?.id) {
-        map.set(options.id, ctx);
-      }
+): Promise<AudioContext> {
+  if (options?.id && map.has(options.id)) {
+    const ctx = map.get(options.id);
+    if (ctx) {
+      unlockAudioContext(ctx);
       return ctx;
     }
-  };
-})();
+  }
+  const ctx = new AudioContext(options);
+  if (options?.id) {
+    map.set(options.id, ctx);
+  }
+  unlockAudioContext(ctx);
+  return ctx;
+}
 
 export const blobToJSON = (blob: Blob) =>
   new Promise((resolve, reject) => {
