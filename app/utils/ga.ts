786ac0947c62df4349a95fc332f7b49fa7e7d4cf
Ted <2508067350@qq.com>
2025-07-29 17:06:33 +0800

    Update ga.ts

--- app/utils/ga.ts
+++ app/utils/ga.ts
@@ -1,5 +1,3 @@
-import ReactGA from "react-ga4";
-
 export function trackEvent(
   action: string,
   params: Record<string, any>,
@@ -11,7 +9,5 @@ export function trackEvent(
 
   if (debug) eventParams.debug_mode = true;
 
-  // window.gtag("event", action, eventParams);
-
-  ReactGA.event(action, eventParams);
+  window.gtag("event", action, eventParams);
 }
