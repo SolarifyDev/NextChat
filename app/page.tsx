7c2eb515883601f3f2361dd07892c50a712f833b
Ted <2508067350@qq.com>
2025-07-31 15:44:19 +0800

    添加GrowingIO入口

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/home";
 import { getServerSideConfig } from "./config/server";
+import { GrowingIOProvider } from "./hook/use-growing-io";
 
 const serverConfig = getServerSideConfig();
 
 export default async function App() {
   return (
     <>
+      <GrowingIOProvider />
       <Home />
       {serverConfig?.isVercel && (
         <>
