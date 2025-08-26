5dd791bbc56ffecfdfdf734a29df093f7883caf4
Ted <2508067350@qq.com>
2025-08-26 20:09:40 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
 import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/home";
 import { getServerSideConfig } from "./config/server";
-import dynamic from "next/dynamic";
+import { ArmsProvider } from "./hook/arms-provider";
 
 const serverConfig = getServerSideConfig();
-
-const ArmsProvider = dynamic(
-  async () => (await import("./hook/arms-provider")).ArmsProvider,
-  {
-    ssr: false,
-  },
-);
 
 export default async function App() {
   return (
