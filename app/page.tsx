d3e21b0ed50b7080a7b7923490e6840b62855428
Ted <2508067350@qq.com>
2025-08-26 19:47:09 +0800

    添加引用

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/home";
 import { getServerSideConfig } from "./config/server";
+import { ArmsProvider } from "./hook/arms-provider";
 
 const serverConfig = getServerSideConfig();
 
 export default async function App() {
   return (
     <>
+      <ArmsProvider />
       <Home />
       {serverConfig?.isVercel && (
         <>
